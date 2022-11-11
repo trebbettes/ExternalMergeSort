991bfe72b62feb1945c845d73e28e93e6d76c70d
trebb <trebb@DESKTOP-0QIAUI5>
2022-11-11 23:18:25 +0000

    Add project files.

---
+++ ExternalMergeSort/ExternalMergeSort.cs
@@ -0,0 +1,205 @@
+﻿namespace Tsrx
+{
+    /// <summary>
+    /// Class to merge & sort multiple text pre-sorted text files (e.g. log files).
+    /// Performs the merge/sort on disk in temporary files.
+    /// The temporary StreamReader provided in the merge callback is disposed after the call back completes and temporary files are deleted.
+    /// </summary>
+    public sealed class ExternalMergeSort
+    {
+        public sealed class Settings
+        {
+            internal string Id { get; } = Guid.NewGuid().ToString();
+
+            public List<string> FileNames { get; set; } = new List<string>();
+
+            public Func<string, bool>? LineFilter { get; set; } = null;
+
+            public string TempDirectory { get; set; } = $"{Path.GetTempPath()}MergeSort";
+
+            public Action<double>? ProgressHandler { get; set; }
+
+            public IComparer<string> Comparer { get; set; } = StringComparer.InvariantCulture;
+        }
+
+        private readonly Settings _settings;
+        private readonly double _progressIncrement;
+
+        private double _progress;
+        private int _i = 0;
+
+        public ExternalMergeSort(Settings settings)
+        {
+            _settings = settings;
+            _progressIncrement = 1 / GetTotalMerges();
+        }
+
+        public void Merge(Action<StreamReader> then) 
+        {
+            Directory.CreateDirectory(_settings.TempDirectory);
+
+            if (_settings.FileNames.Count == 0) return;
+
+            var result = MergeSort(0, _settings.FileNames.Count - 1);
+
+            then.Invoke(result.GetResults());
+
+            result.Dispose();
+        }
+
+        private double GetTotalMerges()
+        {
+            var total = 0;
+            var last = _settings.FileNames.Count;
+            while (last > 0)
+            {
+                total += last;
+                last /= 2;
+            }
+            return total + 1;
+        }
+
+        private MergeReader MergeSort(int min, int max)
+        {
+            _settings.ProgressHandler?.Invoke(_progress += _progressIncrement);
+
+            if (min == max) return new MergeReader(_settings.FileNames[min], _settings);
+
+            var mid = (max - min) / 2 + min;
+
+            var left = MergeSort(min, mid);
+            var right = MergeSort(mid + 1, max);
+
+            if (left is null)
+            {
+                return right;
+            }
+
+            if (right is null)
+            {
+                return left;
+            }
+            
+            var result = Merge(left, right);
+
+            return result;
+        }
+
+        private string GetFileName() {
+            return $"{_settings.TempDirectory}/{_settings.Id}.{_i++}.tmp";
+        }
+
+        private MergeReader Merge(MergeReader left, MergeReader right)
+        {
+            var tempFileName = GetFileName();
+            var tempFile = File.CreateText(tempFileName);
+
+            while (!left.Finished && !right.Finished)
+            {
+                if (_settings.Comparer.Compare(left.Peek(), right.Peek()) <= 0)
+                {
+                    tempFile.WriteLine(left.Read());
+                    continue;
+                }
+
+                tempFile.WriteLine(right.Read());
+            }
+
+            if (!left.Finished)
+            {
+                left.CopyTo(tempFile);
+            }
+
+            if (!right.Finished)
+            {
+                right.CopyTo(tempFile);
+            }
+
+            tempFile.Close();
+            left.Dispose();
+            right.Dispose();
+
+            return new MergeReader(tempFileName, _settings);
+        }
+
+        private sealed class MergeReader
+        {
+            public bool Finished => _currentLine is null;
+
+            private readonly string _file;
+            private readonly Settings _settings;
+            private readonly StreamReader _stream;
+
+            private string? _currentLine;
+
+            public MergeReader(string file, Settings settings)
+            {
+                _file = file;
+                _stream = File.OpenText(file);
+                _settings = settings;
+                MoveNext();
+            }
+
+            public string? Peek()
+            {
+                return _currentLine;
+            }
+
+            public string? Read()
+            {
+                var line = _currentLine;
+                MoveNext();
+                return line;
+            }
+
+            public void Dispose()
+            {
+                _stream.Close();
+                if (_file.Contains(_settings.Id)) File.Delete(_file);
+            }
+
+            public StreamReader GetResults()
+            {
+
+                _stream.DiscardBufferedData();
+                _stream.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                return _stream;
+            }
+
+            public void CopyTo(StreamWriter stream)
+            {
+                while (!Finished)
+                {
+                    stream.WriteLine(_currentLine);
+                    MoveNext();
+                }
+            }
+
+            public StreamReader Complete()
+            {
+                _stream.Close();
+                return File.OpenText(_file);
+            }
+
+            private void MoveNext()
+            {
+                if (_settings.LineFilter is null)
+                {
+                    _currentLine = _stream.ReadLine();
+                    return;
+                }
+
+                while (!Finished || !_stream.EndOfStream)
+                {
+                    var line = _stream.ReadLine();
+                    
+                    if (line is not null && !_settings.LineFilter.Invoke(line)) continue;
+                    
+                    _currentLine = line;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/ExternalMergeSortTests.cs
@@ -0,0 +1,181 @@
+using Tsrx;
+using Xunit;
+
+namespace Tests
+{
+    public class ExternalMergeSortTests
+    {
+        private readonly List<string> _fileNames;
+        private readonly string _lines = "abcdefghijklmnopqrstuvwxyz";
+        private readonly int _fileCount = 10;
+
+        public ExternalMergeSortTests()
+        {
+            var dir = $"{Path.GetTempPath()}MergeSortTests";
+            
+            if (Directory.Exists(dir)) Directory.Delete(dir, true);
+
+            Directory.CreateDirectory(dir);
+            
+            var files = new List<string>();
+
+            for (var i = 0; i < _fileCount; i++)
+            {
+                var fileName = $"{dir}\\test{i}.txt";
+                var file = File.CreateText(fileName);
+                files.Add(fileName);
+                //var values = i % 2 == 0 ? lines.Reverse() : lines;
+                foreach (var c in _lines) file.WriteLine(c);
+                file.Close();
+            }
+
+            _fileNames = files;
+        }
+
+        [Fact]
+        public void ResultsStreamShouldBeAtStart()
+        {
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+            });
+
+            ms.Merge(results =>
+            {
+                Assert.Equal(0, results.BaseStream.Position);
+            });
+        }
+
+        [Fact]
+        public void AllLinesShouldAppearInResults()
+        {
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+            });
+
+            ms.Merge(results =>
+            {
+                var lines = 0;
+                while (results.ReadLine() != null)
+                {
+                    lines++;
+                }
+                Assert.Equal(_fileCount * _lines.Length, lines);
+            });
+        }
+
+
+        [Fact]
+        public void AllResultsShouldBeInCorrectOrder()
+        {
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+            });
+
+            ms.Merge(results =>
+            {
+                var lastValue = 0;
+                while (true)
+                {
+                    var value = results.ReadLine();
+                    if (value is null)
+                    {
+                        Assert.True(true);
+                        return;
+                    }
+                    if (value[0] < lastValue)
+                    {
+                        Assert.False(true);
+                    }
+                    lastValue = value[0];
+                }
+            });
+        }
+
+
+        [Fact]
+        public void FilteredLinesShouldNotBeIncludedInResults()
+        {
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+                LineFilter = (line) => !line.Contains('a')
+            });
+
+            ms.Merge(results =>
+            {
+                var lines = 0;
+                while (results.ReadLine() != null)
+                {
+                    lines++;
+                }
+                Assert.Equal(_fileCount * _lines.Length - 10, lines);
+            });
+        }
+
+        [Fact]
+        public void ProgressHandlerShouldGiveExpectedResults()
+        {
+            var progress = new List<double>();
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+                ProgressHandler = val => progress.Add(val)
+            });
+
+            ms.Merge(results => { });
+
+            Assert.Equal(19, progress.Count);
+            Assert.True(progress.Last() > 0.99);
+        }
+
+
+        [Fact]
+        public void TemporaryFilesShouldBeCreatedInTheCorrectPlace()
+        {
+            var dir = $"{Directory.GetCurrentDirectory()}/MergeTest";
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+                TempDirectory = dir
+            });
+
+            ms.Merge(results =>
+            {
+                Assert.True(Directory.GetFiles(dir).Length == 1);
+            });
+        }
+
+
+        [Fact]
+        public void TemporaryFilesShouldBeDeleted()
+        {
+            var dir = $"{Directory.GetCurrentDirectory()}/MergeTest";
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+                TempDirectory = dir
+            });
+
+            ms.Merge(results => { });
+
+            Assert.True(Directory.GetFiles(dir).Length == 0);
+        }
+
+
+        [Fact]
+        public void SourceFilesShouldNotBeDelted()
+        {
+            var ms = new ExternalMergeSort(new ExternalMergeSort.Settings
+            {
+                FileNames = _fileNames,
+            });
+
+            ms.Merge(results => { });
+
+            Assert.True(_fileNames.All(f => File.Exists(f)));
+        }
+    }
+}
